706b7d8464679550b84ba6ec172632ad640db51b
HeliumAnt <lesgoets@gmail.com>
2023-12-27 21:11:56 +0100

    move draw to renderer

--- Source/Renderer/Interface/RendererInterface.h
+++ Source/Renderer/Interface/RendererInterface.h
@@ -11,6 +11,18 @@ namespace RTE {
 		virtual void Clear() = 0;
 		virtual void Draw(RenderState&& state) = 0;
 		virtual void Render() = 0;
+		virtual int Draw(Texture2D *texture, float x, float y) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, glm::vec3 color) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, float angle) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, glm::vec2 flip) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, float angle, glm::vec2 flip) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, float w, float h) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, float w, float h, float angle) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, float w, float h, float angle, glm::vec2 flip) = 0;
+		virtual int Draw(Texture2D *texture, float x, float y, float w, float h, float angle, glm::vec2 flip, glm::vec3 color) = 0;
+		virtual int Draw(Shape *shape, float x, float y) = 0;
+		virtual int Draw(Shape *shape, float x, float y, float angle) = 0;
+		virtual int Draw(Shape *shape, float x, float y, float angle, glm::vec3 color) = 0;
 	};
 } // namespace RTE
 
