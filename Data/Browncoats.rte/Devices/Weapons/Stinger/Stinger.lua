1399a464c4f8e3b4f05f721ba2f940e2bdb89b95
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-01-07 03:08:04 +0200

    inexplicably broken MuzzleSmoke replacement

--- Data/Browncoats.rte/Devices/Weapons/Stinger/Stinger.lua
+++ Data/Browncoats.rte/Devices/Weapons/Stinger/Stinger.lua
@@ -1,12 +1,6 @@
 function OnFire(self)
 
 	self.mechSound:Play(self.Pos);
-	local flip = self.HFlipped and math.pi or 0;
-	local angle = self.RotAngle + flip;
-	self.fireSmokeEffect.Position = self.MuzzlePos;
-	self.fireSmokeEffect.RadAngle = angle;
-
-	self.particleUtility:CreateDirectionalSmokeEffect(self.fireSmokeEffect);
 
 end
 
@@ -14,20 +8,5 @@ function Create(self)
 
 	self.mechSound = CreateSoundContainer("Mech Browncoat HG-10", "Browncoats.rte");
 	
-	self.fireSmokeEffect = {};
-	self.fireSmokeEffect.Position = self.Pos;
-	self.fireSmokeEffect.Source = self;
-	self.fireSmokeEffect.RadAngle = self.RotAngle;
-	self.fireSmokeEffect.Power = 15;
-	self.fireSmokeEffect.Spread = 15;
-	self.fireSmokeEffect.SmokeMult = 1;
-	self.fireSmokeEffect.ExploMult = 1;
-	self.fireSmokeEffect.WidthSpread = 2;
-	self.fireSmokeEffect.VelocityMult = 1.5;
-	self.fireSmokeEffect.LingerMult = 0.5;
-	self.fireSmokeEffect.AirResistanceMult = 1;
-	self.fireSmokeEffect.GravMult = 0.5;
-	
-	self.particleUtility = require("Scripts/Utility/ParticleUtility");
 	
 end
