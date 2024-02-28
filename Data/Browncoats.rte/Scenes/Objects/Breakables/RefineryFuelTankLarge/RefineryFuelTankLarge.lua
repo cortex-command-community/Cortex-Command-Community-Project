673d1727380cee59294aca2c9228f190529124ec
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-02-29 01:04:56 +0200

    fuel tank sounds

--- Data/Browncoats.rte/Scenes/Objects/Breakables/RefineryFuelTankLarge/RefineryFuelTankLarge.lua
+++ Data/Browncoats.rte/Scenes/Objects/Breakables/RefineryFuelTankLarge/RefineryFuelTankLarge.lua
@@ -1,11 +1,14 @@
 function Create(self)
 	self.unpinThreshold = self.GibWoundLimit * 0.5;
+	self.plummetSound = CreateSoundContainer("Browncoat Refinery Fuel Tank Large Plummet", "Browncoats.rte");
 end
 
 function Update(self)
+	self.plummetSound.Pos = self.Pos;
 	if self.WoundCount > self.unpinThreshold and self.PinStrength > 0 then
 		self.PinStrength = 0;
 		
+		self.plummetSound:Play(self.Pos);
 		self.AngularVel = math.random(-1, 1);
 		self.Vel = self.Vel + Vector(math.random(-1, 1), 0);
 	end
@@ -13,4 +16,14 @@ end
 
 function OnCollideWithTerrain(self)
 	self:GibThis();
+end
+
+function OnCollideWithMO(self, MO, rootMO)
+	if self.PinStrength == 0 and MO.UniqueID == rootMO.UniqueID and IsMOSRotating(MO) then
+		ToMOSRotating(MO):GibThis();
+	end
+end
+
+function OnDestroy(self)
+	self.plummetSound:FadeOut(200);
 end
--- Data/Browncoats.rte/Scenes/Objects/Breakables/RefineryFuelTankMedium/RefineryFuelTankMedium.lua
+++ Data/Browncoats.rte/Scenes/Objects/Breakables/RefineryFuelTankMedium/RefineryFuelTankMedium.lua
@@ -1,14 +1,16 @@
 function Create(self)
 	self.unpinThreshold = self.GibWoundLimit * 0.5;
+	self.plummetSound = CreateSoundContainer("Browncoat Refinery Fuel Tank Medium Plummet", "Browncoats.rte");
 end
 
 function Update(self)
+	self.plummetSound.Pos = self.Pos;
 	if self.WoundCount > self.unpinThreshold and self.PinStrength > 0 then
 		self.PinStrength = 0;
 		
+		self.plummetSound:Play(self.Pos);
 		self.AngularVel = math.random(-1, 1);
 		self.Vel = self.Vel + Vector(math.random(-1, 1), 0);
-		
 	end
 end
 
@@ -17,11 +19,11 @@ function OnCollideWithTerrain(self)
 end
 
 function OnCollideWithMO(self, MO, rootMO)
-
-	if self.PinStrength == 0 and MO.UniqueID == rootMO.UniqueID then
-		print(MO)
-		print(rootMO)
+	if self.PinStrength == 0 and MO.UniqueID == rootMO.UniqueID and IsMOSRotating(MO) then
 		ToMOSRotating(MO):GibThis();
 	end
-	
+end
+
+function OnDestroy(self)
+	self.plummetSound:FadeOut(200);
 end
