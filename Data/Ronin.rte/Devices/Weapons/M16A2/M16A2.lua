4a237092c719087e361b388c2da398ffe47b58c5
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-06-01 03:13:29 +0300

    better m16 sounds

--- Data/Ronin.rte/Devices/Weapons/M16A2/M16A2.lua
+++ Data/Ronin.rte/Devices/Weapons/M16A2/M16A2.lua
@@ -6,14 +6,20 @@ function OnFire(self)
 	local parent = self:GetRootParent();
 	if IsActor(parent) and ToActor(parent):IsPlayerControlled() then
 		if self.RoundInMagCount < 11 then
-			self.mechSound.Pitch = 1.2 + (11 - self.RoundInMagCount)/100;
+			self.mechSound.Pitch = 1.1 + (11 - self.RoundInMagCount)/100;
 			self.mechSound.Volume = 1.3 + (11 - self.RoundInMagCount)/25;
 		end
 	end
 	
 	self.mechSound:Play(self.Pos);
+	
+	if self.RoundInMagCount == 0 then
+		self.lastRoundSound:Play(self.Pos);
+	end
 end
 
 function Create(self)
 	self.mechSound = CreateSoundContainer("Mech Ronin M16A2", "Ronin.rte");
+	self.lastRoundSound = CreateSoundContainer("Last Round Ronin", "Ronin.rte");
+	self.lastRoundSound.Pitch = 1.0;
 end
