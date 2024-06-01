c732e5989773c1453589e59a41cb1f2205d95ba9
Causeless <tommymarch5@gmail.com>
2024-06-01 17:15:42 +0100

    Fixed fog resolution in refinery assault

--- Data/Browncoats.rte/Activities/RefineryAssaultFunctions.lua
+++ Data/Browncoats.rte/Activities/RefineryAssaultFunctions.lua
@@ -985,7 +985,8 @@ function RefineryAssault:SetupFirstStage()
 
 	-- Fog of war-causing callback if it's enabled, but none if it's not
 	if self:GetFogOfWarEnabled() then
-		self.HUDHandler:QueueCameraPanEvent(self.humanTeam, "S1IntroPan4", cameraPos, 0.01, 5000, true, true, true, function() SceneMan:MakeAllUnseen(Vector(1, 1), self.humanTeam); end);
+		local fogResolution = 4;
+		self.HUDHandler:QueueCameraPanEvent(self.humanTeam, "S1IntroPan4", cameraPos, 0.01, 5000, true, true, true, function() SceneMan:MakeAllUnseen(Vector(fogResolution, fogResolution), self.humanTeam); end);
 	else
 		self.HUDHandler:QueueCameraPanEvent(self.humanTeam, "S1IntroPan4", cameraPos, 0.01, 5000, true, true, true);
 	end
