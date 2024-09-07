1c79296d2de1daea3594b0420c97baeebe1e1f93
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-09-07 14:40:25 +0300

    implement some main objective ambient loops

--- Data/Browncoats.rte/Scenes/Objects/Breakables/RefineryConsole/RefineryConsole.lua
+++ Data/Browncoats.rte/Scenes/Objects/Breakables/RefineryConsole/RefineryConsole.lua
@@ -2,6 +2,8 @@ function Create(self)
 
 	self.Activity = ToGameActivity(ActivityMan:GetActivity());
 	
+	self.loopSound = CreateSoundContainer("Yskely Refinery Refinery Console Loop", "Browncoats.rte");
+	self.loopSound:Play(self.Pos);
 end
 
 function OnDestroy(self)
