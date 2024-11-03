a492aa015b837c9c2e1999ed4c85e077924bf04d
HeliumAnt <lesgoets@gmail.com>
2024-11-03 01:02:55 +0100

    add BITMAP draw convenience functions

--- Source/Renderer/Draw.h
+++ Source/Renderer/Draw.h
@@ -4,4 +4,12 @@
 
 struct BITMAP;
 namespace RTE {
+
+	void DrawTexture(BITMAP* bitmap, int posX, int posY, RLColor tint);
+
+	void DrawTextureV(BITMAP* bitmap, Vector2 pos, RLColor tint);
+
+	void DrawTextureEx(BITMAP* bitmap, Vector2 pos, float rotation, float scale, RLColor tint);
+	void DrawTextureRec(BITMAP* bitmap, Rectangle source, Vector2 pos, RLColor tint);
+	void DrawTexturePro(BITMAP* bitmap, Rectangle source, Rectangle dest, Vector2 origin, float rotation, RLColor tint);
 }
