ee7802f12335beef94b8f888e69feac4a6408f38
HeliumAnt <lesgoets@gmail.com>
2024-11-16 22:03:43 +0100

    fix glow strength, fr this time

--- Source/Managers/PostProcessMan.cpp
+++ Source/Managers/PostProcessMan.cpp
@@ -436,6 +436,7 @@ void PostProcessMan::DrawPostScreenEffects() {
 	GL_CHECK(glBindVertexArray(m_VertexArray));
 	m_PostProcessShader->SetInt(m_PostProcessShader->GetTextureUniform(), 0);
 	m_PostProcessShader->SetMatrix4f(m_PostProcessShader->GetProjectionUniform(), *m_ProjectionMatrix);
+	m_PostProcessShader->SetVector4f(m_PostProcessShader->GetColorUniform(), glm::vec4(1.0f));
 
 	for (const PostEffect& postEffect: m_PostScreenEffects) {
 		if (postEffect.m_Bitmap) {
@@ -443,7 +444,7 @@ void PostProcessMan::DrawPostScreenEffects() {
 			effectStrength = postEffect.m_Strength;
 			effectPosX = postEffect.m_Pos.m_X - postEffect.m_Bitmap->w / 2;
 			effectPosY = postEffect.m_Pos.m_Y - postEffect.m_Bitmap->h / 2;
-			DrawTexture(g_GLResourceMan.GetStaticTextureFromBitmap(postEffect.m_Bitmap), effectPosX, effectPosY, {.r=effectStrength, .g=effectStrength, .b=effectStrength, .a=effectStrength});
+			DrawTexture(g_GLResourceMan.GetStaticTextureFromBitmap(postEffect.m_Bitmap), effectPosX, effectPosY, {.r=effectStrength, .g=effectStrength, .b=effectStrength, .a=255});
 		}
 	}
 }
