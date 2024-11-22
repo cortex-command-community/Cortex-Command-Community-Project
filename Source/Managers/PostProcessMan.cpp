d94cb283297669b976146b8f95ebeadec79bcf28
HeliumAnt <lesgoets@gmail.com>
2024-11-22 18:01:07 +0100

    fix postprocess layer not being cleared

--- Source/Managers/PostProcessMan.cpp
+++ Source/Managers/PostProcessMan.cpp
@@ -349,7 +349,7 @@ void PostProcessMan::PostProcess() {
 	UpdatePalette();
 
 	// First copy the current 8bpp backbuffer to the 32bpp buffer; we'll add effects to it
-	m_PostProcessFramebuffer->Begin(false);
+	m_PostProcessFramebuffer->Begin(true);
 	//m_Blit8->Begin();
 	//int paletteUniform = m_Blit8->GetUniformLocation("rtePalette");
 	//rlSetUniformSampler(paletteUniform, m_Palette8Texture);
