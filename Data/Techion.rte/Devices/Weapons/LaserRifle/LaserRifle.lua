0f8461820db2c5cd12f1a05d86897da8ec924044
comradeshook <comradeshook@gmail.com>
2024-12-07 13:58:15 +0100

    Optimise laser rifle

--- Data/Techion.rte/Devices/Weapons/LaserRifle/LaserRifle.lua
+++ Data/Techion.rte/Devices/Weapons/LaserRifle/LaserRifle.lua
@@ -1,36 +1,51 @@
-function Create(self)
-	self.range = math.sqrt(FrameMan.PlayerScreenWidth^2 + FrameMan.PlayerScreenHeight^2)/2;
-	self.penetrationStrength = 170;
-	self.strengthVariation = 5;
-	--This value tracks the shots and varies the penetration strength to create a "resistance" effect on tougher materials
-	self.shotCounter = 0;	--TODO: Rename/describe this variable better
-	self.activity = ActivityMan:GetActivity();
+local function InsertParticle(var, particle)
+	var.addedParticleCount = var.addedParticleCount + 1;
+	var.addedParticles[var.addedParticleCount] = particle;
+end
 
-	self.cooldown = Timer();
-	self.cooldownSpeed = 0.5;
-
-	self.addedParticles = {};
-
-	self.addedWound = nil;
-	self.addedWoundOffset = nil;
-	self.addedWoundToUniqueID = nil;
-
-	function self.emitSmoke(particleCount)
-		for i = 1, particleCount do
-			local smoke = CreateMOSParticle("Tiny Smoke Ball 1" .. (math.random() < 0.5 and " Glow Blue" or ""), "Base.rte");
-			smoke.Pos = self.MuzzlePos;
-			smoke.Lifetime = smoke.Lifetime * RangeRand(0.5, 1.0);
-			smoke.Vel = self.Vel * 0.5 + Vector(RangeRand(0, i), 0):RadRotate(RangeRand(-math.pi, math.pi));
-			table.insert(self.addedParticles, smoke);
-		end
-		self:RequestSyncedUpdate();
+local function emitSmoke(self, particleCount)
+	local var = self.var;
+	local MuzzlePos = self.MuzzlePos;
+	for i = 1, particleCount do
+		local smoke = CreateMOSParticle("Tiny Smoke Ball 1" .. (math.random() < 0.5 and " Glow Blue" or ""), "Base.rte");
+		smoke.Pos = MuzzlePos;
+		smoke.Lifetime = smoke.Lifetime * RangeRand(0.5, 1.0);
+		smoke.Vel = var.Vel * 0.5 + Vector(RangeRand(0, i), 0):RadRotate(RangeRand(-math.pi, math.pi));
+		InsertParticle(var, smoke);
 	end
+	self:RequestSyncedUpdate();
+end
+
+function Create(self)
+	-- Create local table to store variables for performance
+	local var = {};
+
+	var.range = math.sqrt(FrameMan.PlayerScreenWidth^2 + FrameMan.PlayerScreenHeight^2)/2;
+	var.penetrationStrength = 170;
+	var.strengthVariation = 5;
+	--This value tracks the shots and varies the penetration strength to create a "resistance" effect on tougher materials
+	var.shotCounter = 0;	--TODO: Rename/describe this variable better
+	var.activity = ActivityMan:GetActivity();
+
+	var.cooldown = Timer();
+	var.cooldownSpeed = 0.5;
+
+	var.addedParticles = {};
+	var.addedParticleCount = 0;
+
+	var.addedWound = nil;
+	var.addedWoundOffset = nil;
+	var.addedWoundToUniqueID = nil;
+	var.Vel = self.Vel;
+
+	self.var = var;
 end
 
 function ThreadedUpdate(self)
+	local var = self.var;
 	if self.FiredFrame then
 		local actor = self:GetRootParent();
-		local range = self.range + math.random(8);
+		local range = var.range + math.random(8);
 		if IsActor(actor) then
 			actor = ToActor(actor);
 			range = range + actor.AimDistance;
@@ -52,12 +67,12 @@ function ThreadedUpdate(self)
 			skipPx = 1;
 			local shortRay = SceneMan:CastObstacleRay(gapPos, Vector(trace.X, trace.Y):SetMagnitude(range - rayLength + skipPx), hitPos, gapPos, actor.ID, self.Team, rte.airID, skipPx);
 			gapPos = gapPos - Vector(trace.X, trace.Y):SetMagnitude(skipPx);
-			local strengthFactor = math.max(1 - rayLength/self.range, math.random()) * (self.shotCounter + 1)/self.strengthVariation;
+			local strengthFactor = math.max(1 - rayLength/var.range, math.random()) * (var.shotCounter + 1)/var.strengthVariation;
 
-			self.addedWoundToMOID = SceneMan:GetMOIDPixel(hitPos.X, hitPos.Y);
-			if self.addedWoundToMOID ~= rte.NoMOID and self.addedWoundToMOID ~= self.ID then
-				local mo = ToMOSRotating(MovableMan:GetMOFromID(self.addedWoundToMOID));
-				if self.penetrationStrength * strengthFactor >= mo.Material.StructuralIntegrity then
+			local addedWoundToMOID = SceneMan:GetMOIDPixel(hitPos.X, hitPos.Y);
+			if addedWoundToMOID ~= rte.NoMOID and addedWoundToMOID ~= self.ID then
+				local mo = ToMOSRotating(MovableMan:GetMOFromID(addedWoundToMOID));
+				if var.penetrationStrength * strengthFactor >= mo.Material.StructuralIntegrity then
 					local moAngle = -mo.RotAngle * mo.FlipFactor;
 
 					local woundName = mo:GetEntryWoundPresetName();
@@ -68,9 +83,9 @@ function ThreadedUpdate(self)
 						local woundOffset = Vector(dist.X * mo.FlipFactor, dist.Y):RadRotate(moAngle):SetMagnitude(dist.Magnitude - (wound.Radius - 1) * wound.Scale);
 						wound.InheritedRotAngleOffset = woundOffset.AbsRadAngle;
 						woundOffset = woundOffset:RadRotate(-mo.RotAngle);
-						self.addedWound = wound;
-						self.addedWoundOffset = woundOffset;
-						self.addedWoundToUniqueID = mo.UniqueID;
+						var.addedWound = wound;
+						var.addedWoundOffset = woundOffset;
+						var.addedWoundToUniqueID = mo.UniqueID;
 						self:RequestSyncedUpdate();
 					end
 				end
@@ -80,19 +95,19 @@ function ThreadedUpdate(self)
 			smoke.Pos = gapPos;
 			smoke.Vel = Vector(-trace.X, -trace.Y):SetMagnitude(math.random(3, 6)):RadRotate(RangeRand(-1.5, 1.5));
 			smoke.Lifetime = smoke.Lifetime * strengthFactor;
-			table.insert(self.addedParticles, smoke);
+			InsertParticle(var, smoke);
 
 			local pix = CreateMOPixel("Laser Rifle Glow " .. math.floor(strengthFactor * 4 + 0.5), "Techion.rte");
 			pix.Pos = gapPos;
-			pix.Sharpness = self.penetrationStrength/6;
+			pix.Sharpness = var.penetrationStrength/6;
 			pix.Vel = Vector(trace.X, trace.Y):SetMagnitude(6);
-			table.insert(self.addedParticles, pix);
+			InsertParticle(var, pix);
 		end
 		if rayLength ~= 0 then
 			trace = SceneMan:ShortestDistance(startPos, gapPos, SceneMan.SceneWrapsX);
 			for player = Activity.PLAYER_1, Activity.MAXPLAYERCOUNT - 1 do
-				local team = self.activity:GetTeamOfPlayer(player);
-				local screen = self.activity:ScreenOfPlayer(player);
+				local team = var.activity:GetTeamOfPlayer(player);
+				local screen = var.activity:ScreenOfPlayer(player);
 				if screen ~= -1 and not (SceneMan:IsUnseen(startPos.X, startPos.Y, team) or SceneMan:IsUnseen(hitPos.X, hitPos.Y, team)) then
 					PrimitiveMan:DrawLinePrimitive(screen, startPos, startPos + trace, 254);
 				end
@@ -101,48 +116,55 @@ function ThreadedUpdate(self)
 			for i = 0, particleCount do
 				local pix = CreateMOPixel("Laser Rifle Glow 0", "Techion.rte");
 				pix.Pos = startPos + trace * i/particleCount;
-				pix.Vel = self.Vel;
-				table.insert(self.addedParticles, pix);
+				pix.Vel = var.Vel;
+				InsertParticle(var, pix);
 			end
 		end
-		self.shotCounter = (self.shotCounter + 1) % self.strengthVariation;
-		self.cooldown:Reset();
+		var.shotCounter = (var.shotCounter + 1) % var.strengthVariation;
+		var.cooldown:Reset();
 	end
 
 	if self.Magazine and self.Magazine.RoundCount > 0 then
 		local ammoRatio = 1 - self.Magazine.RoundCount/self.Magazine.Capacity;
-		self.emitSmoke(math.floor(ammoRatio * RangeRand(0.5, 2.0) + RangeRand(0.25, 0.50)));
-		self.cooldown:SetSimTimeLimitMS(self.ReloadTime * ammoRatio);
+		emitSmoke(self, math.floor(ammoRatio * RangeRand(0.5, 2.0) + RangeRand(0.25, 0.50)));
+		var.cooldown:SetSimTimeLimitMS(self.ReloadTime * ammoRatio);
 
-		local cooldownRate = math.floor(self.cooldown.ElapsedSimTimeMS/(60000/(self.RateOfFire * self.cooldownSpeed)));
+		local cooldownRate = math.floor(var.cooldown.ElapsedSimTimeMS/(60000/(self.RateOfFire * var.cooldownSpeed)));
 		if ammoRatio ~= 0 and cooldownRate >= 1 then
 			self.Magazine.RoundCount = math.min(self.Magazine.RoundCount + cooldownRate, self.Magazine.Capacity);
-			self.cooldown:Reset();
+			var.cooldown:Reset();
 		end
 		self.FireSound.Pitch = (1.0 - ammoRatio * 0.1)^2;
 	elseif self:IsReloading() then
-		self.emitSmoke(math.floor((self.cooldown:LeftTillSimTimeLimitMS()/self.ReloadTime) * RangeRand(0.5, 2.0) + RangeRand(0.50, 0.75)));
+		emitSmoke(self, math.floor((var.cooldown:LeftTillSimTimeLimitMS()/self.ReloadTime) * RangeRand(0.5, 2.0) + RangeRand(0.50, 0.75)));
 	elseif self.RoundInMagCount >= 0 then
 		self:Reload();
 	end
 end
 
+-- Localize function to improve performance
+local AddParticle = MovableMan.AddParticle;
+
 function SyncedUpdate(self)
-	if self.addedWound then
-		local mo = MovableMan:FindObjectByUniqueID(self.addedWoundToUniqueID);
+	local var = self.var;
+	if var.addedWound then
+		local mo = MovableMan:FindObjectByUniqueID(var.addedWoundToUniqueID);
 		if mo then
 			mo = ToMOSRotating(mo);
-			mo:AddWound(self.addedWound, self.addedWoundOffset, true);
+			mo:AddWound(var.addedWound, var.addedWoundOffset, true);
 		end
 		
-		self.addedWound = nil;
-		self.addedWoundOffset = nil;
-		self.addedWoundToMOID = nil;
-		self.addedWoundToUniqueID = nil;
+		var.addedWound = nil;
+		var.addedWoundOffset = nil;
+		var.addedWoundToUniqueID = nil;
 	end
 
-	for i = 1, #self.addedParticles do
-		MovableMan:AddParticle(self.addedParticles[i]);
+	if (var.addedParticleCount > 0) then
+		for i = 1, var.addedParticleCount do
+			-- Remember to add function caller as first argument to localized function
+			AddParticle(MovableMan, var.addedParticles[i]);
+		end
+		var.addedParticles = {};
+		var.addedParticleCount = 0;
 	end
-	self.addedParticles = {};
 end
