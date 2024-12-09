f83fec12d5566fbcbf13b8e9e10912d50d0b9ae5
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-12-09 03:19:04 +0200

    actually properly bind CastAllMOsRay

--- Source/Lua/LuaBindingsManagers.cpp
+++ Source/Lua/LuaBindingsManagers.cpp
@@ -330,7 +330,7 @@ LuaBindingRegisterFunctionDefinitionForType(ManagerLuaBindings, SceneMan) {
 	    .def("CastWeaknessRay", &SceneMan::CastWeaknessRay)
 	    .def("CastMORay", &LuaAdaptersSceneMan::CastMORay1)
 	    .def("CastMORay", &LuaAdaptersSceneMan::CastMORay2)
-	    .def("CastAllMOsRay", &LuaAdaptersSceneMan::CastAllMOsRay)
+	    .def("CastAllMOsRay", &LuaAdaptersSceneMan::CastAllMOsRay, luabind::return_stl_iterator)
 	    .def("CastFindMORay", &SceneMan::CastFindMORay)
 	    .def("CastObstacleRay", &LuaAdaptersSceneMan::CastObstacleRay1)
 	    .def("CastObstacleRay", &LuaAdaptersSceneMan::CastObstacleRay2)
