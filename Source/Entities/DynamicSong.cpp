b0c5876ab0b84f8f6295ad7b8e7ea32e428edc9b
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-12-19 21:02:15 +0200

    also move assert up just in case

--- Source/Entities/DynamicSong.cpp
+++ Source/Entities/DynamicSong.cpp
@@ -166,6 +166,8 @@ SoundContainer& DynamicSongSection::SelectTransitionSoundContainer() {
 }
 
 SoundContainer& DynamicSongSection::SelectSoundContainer() {
+	RTEAssert(!m_SoundContainers.empty(), "Tried to get a SoundContainer from a DynamicSongSection with none to choose from!");
+	
 	// Shuffle between our options if we have multiple
 	if (m_SoundContainers.size() != 1) {
 		if (m_ShuffleUnplayedIndices.empty()) {
@@ -198,8 +200,7 @@ SoundContainer& DynamicSongSection::SelectSoundContainer() {
 			}
 		}
 	}
-
-	RTEAssert(!m_SoundContainers.empty(), "Tried to get a SoundContainer from a DynamicSongSection with none to choose from!");
+	
 	return m_SoundContainers[0];
 }
 
