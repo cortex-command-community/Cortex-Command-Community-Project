59ee482a1639186576fb42de63a00b1b74434be7
HeliumAnt <lesgoets@gmail.com>
2025-12-19 15:55:52 +0100

    init gpu primitive reimpl

---
+++ Source/Renderer/Shapes.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Color.h"
+#include "glm/fwd.hpp"
+#include <vector>
+
+namespace RTE {
+	namespace Draw {
+		void Pixel(glm::vec2 position, Color color);
+		void Line(glm::vec2 start, glm::vec2 end, Color color);
+		void Line(glm::vec2 start, glm::vec2 end, float thickness, Color color);
+		void LineStrip(const std::vector<glm::vec2>& points, Color color);
+		void LineStrip(const std::vector<glm::vec2>& points, float thickness, Color color);
+		void LineBezier(glm::vec2 start, glm::vec2 end, Color color);
+		void Circle(glm::vec2 center, float radius, Color color);
+		void CircleSector(glm::vec2 center, float radius, float startAngle, float endAngle, Color color);
+		void CircleLines(glm::vec2 center, float radius, Color color);
+		void CircleLinesSector(glm::vec2 center, float radius, Color color);
+		void Ellipse(glm::vec2 center, float radiusH, float radiusV, Color color);
+		void EllipseLines(glm::vec2 center, float radiusH, float radiusV, Color color);
+		void Ring(glm::vec2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, Color color);
+		void RingLines(glm::vec2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, Color color);
+		void Rectangle(Box rect, Color color);
+		void RectangleLines(Box rect, Color color);
+		void RoundedRectangle(Box rect, float cornerRadius, Color color);
+		void RoundedRectangleLines(Box rect, float cornerRadius, Color color);
+		void RoundedRectangleLines(Box rect, float cornerRadius, float thickness, Color color);
+		void Triangle(glm::vec2 point1, glm::vec2 point2, glm::vec2 point3, Color color);
+		void TriangleLines(glm::vec2 point1, glm::vec2 point2, glm::vec2 point3, Color color);
+		void TriangleStrip(std::vector<glm::vec2> points, Color color);
+		void Polygon(std::vector<glm::vec2> points, Color color);
+	} // namespace Draw
+} // namespace RTE
