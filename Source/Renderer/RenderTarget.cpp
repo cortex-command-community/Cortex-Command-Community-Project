92810fcd6a638ae811d9c2f257be426310939dc6
HeliumAnt <lesgoets@gmail.com>
2026-02-02 21:14:57 +0100

    reimplement with pure GL and wrappers

--- Source/Renderer/RenderTarget.cpp
+++ Source/Renderer/RenderTarget.cpp
@@ -2,88 +2,60 @@
 #include "RTEError.h"
 #include "glad/gl.h"
 #include "GLCheck.h"
-#include "raylib/rlgl.h"
 #include "WindowMan.h"
+#include "RenderMan.h"
 #include "Constants.h"
+#include "Texture.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
 using namespace RTE;
 
-RenderTarget::RenderTarget(const FloatRect& size, const FloatRect& defaultViewport, int bitDepth, Texture2D colorTexture, bool defaultFB0) {
+RenderTarget::RenderTarget(const FloatRect& size, const FloatRect& defaultViewport, int bitDepth, std::shared_ptr<BitmapTexture> colorTexture, bool defaultFB0) {
 	m_Size = size;
 	m_Viewport = defaultViewport;
 	if (!defaultFB0) {
-		if (colorTexture.id != 0) {
-			m_Texture = std::move(colorTexture);
+		if (colorTexture) {
+			m_Texture = colorTexture;
 			m_ColorTextureOwned = false;
 		} else {
-			m_Texture = {
-			    .id = rlLoadTexture(nullptr, size.w, size.h, bitDepth == 8 ? PIXELFORMAT_UNCOMPRESSED_GRAYSCALE : PIXELFORMAT_UNCOMPRESSED_R8G8B8A8, 1),
-			    .width = static_cast<int>(size.w),
-			    .height = static_cast<int>(size.h),
-			    .mipmaps = 0,
-			    .format = bitDepth == 8 ? PIXELFORMAT_UNCOMPRESSED_GRAYSCALE : PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
-			};
+			m_Texture = std::make_shared<Texture>(size, bitDepth);
 		}
-		m_Depth = {
-		    .id = rlLoadTextureDepth(size.w, size.h, false),
-		    .width = static_cast<int>(size.w),
-		    .height = static_cast<int>(size.h),
-		    .mipmaps = 0,
-		    .format = GL_DEPTH_COMPONENT,
-		};
-		m_FBO = rlLoadFramebuffer();
-		rlEnableFramebuffer(m_FBO);
-		rlFramebufferAttach(m_FBO, m_Texture.id, RL_ATTACHMENT_COLOR_CHANNEL0, RL_ATTACHMENT_TEXTURE2D, 0);
-		rlFramebufferAttach(m_FBO, m_Depth.id, RL_ATTACHMENT_DEPTH, RL_ATTACHMENT_TEXTURE2D, 0);
-		rlDisableFramebuffer();
+		m_Depth = std::make_shared<DepthTexture>(size);
+		glGenFramebuffers(1, &m_FBO);
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, m_FBO);
+
+		glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_Texture->GetTextureId(), 0);
+		glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_Depth->GetTextureId(), 0);
+
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 	} else {
 		m_ColorTextureOwned = false;
-		m_Depth.id = 0;
-		m_Texture.id = 0;
+		m_Depth = nullptr;
+		m_Texture = nullptr;
 	}
 }
 
 RenderTarget::~RenderTarget() {
 	if (m_FBO) {
-		rlUnloadFramebuffer(m_FBO);
+		glDeleteFramebuffers(1, &m_FBO);
 	}
-	if (m_ColorTextureOwned) {
-		rlUnloadTexture(m_Texture.id);
-	}
-	rlUnloadTexture(m_Depth.id);
 }
 
 void RenderTarget::Begin(bool clear, float zoom) {
-	rlDrawRenderBatchActive();
-	rlResetDrawDepth();
-	rlEnableFramebuffer(m_FBO);
+	g_RenderMan.DrawActiveBatch();
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, m_FBO);
 
 	glViewport(m_Viewport.x, m_Viewport.y, m_Viewport.w, m_Viewport.h);
 
-	rlSetFramebufferWidth(m_Size.w);
-	rlSetFramebufferHeight(m_Size.h);
-
-	rlMatrixMode(RL_PROJECTION);
-	rlLoadIdentity();
-	rlOrtho(0.0f, m_Size.w, m_Size.h, 0.0f, c_NearDepth, c_FarDepth);
-	rlMatrixMode(RL_MODELVIEW);
-	rlLoadIdentity();
-	rlTranslatef(m_Size.w / 2, m_Size.h / 2, 0.0f);
-	rlScalef(zoom, zoom, 1.0f);
-	rlTranslatef(-m_Size.w / 2, -m_Size.h / 2, 0.0f);
-
 	if (clear) {
-		rlClearScreenBuffers();
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	}
 }
 
 void RenderTarget::End() {
-	rlDrawRenderBatchActive();
-
-	rlDisableFramebuffer();
-	glViewport(0, 0, g_WindowMan.GetResX(), g_WindowMan.GetResY());
-	rlResetDrawDepth();
+	g_RenderMan.DrawActiveBatch();
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 }
--- Source/Renderer/RenderTarget.h
+++ Source/Renderer/RenderTarget.h
@@ -4,6 +4,8 @@
 #include "raylib/raylib.h"
 namespace RTE {
 	class Shader;
+	class Texture;
+	class DepthTexture;
 	class RenderTarget {
 	public:
 
@@ -13,14 +15,14 @@ namespace RTE {
 		/// @param bitDepth The color depth of this target, either 8 or 32 (default 32)
 		/// @param colorTexture Optional color texture, if specified, bitDepth will be ignored.
 		/// @param defaultFB0 Make this target for the window backbuffer.
-		RenderTarget(const FloatRect& size, const FloatRect& defaultViewport, int bitDepth = 32, Texture2D colorTexture = {0, 0, 0, 0, -1}, bool defaultFB0 = false);
+		RenderTarget(const FloatRect& size, const FloatRect& defaultViewport, int bitDepth = 32, std::shared_ptr<BitmapTexture> colorTexture = nullptr, bool defaultFB0 = false);
 		/// Destructor.
 		virtual ~RenderTarget();
-		
+
 		/// Enables this RenderTarget for drawing and sets up projection matrix. Draws current batch, resets draw depth and model matrix.
 		/// @param clear Whether to clear the target.
 		void Begin(bool clear = true, float zoom = 1.0f);
-		
+
 		/// Disables this RenderTarget and flushes the active batch.
 		void End();
 
@@ -28,9 +30,9 @@ namespace RTE {
 		/// @return The FBO.
 		GLuint GetFramebuffer() { return m_FBO; }
 		/// Getter for the color buffer
-		const Texture2D& GetColorTexture() { return m_Texture; }
+		std::weak_ptr<Texture> GetColorTexture() { return m_Texture; }
 		/// Getter for the depth buffer.
-		const Texture2D& GetDepthTexture() { return m_Depth; }
+		std::weak_ptr<DepthTexture> GetDepthTexture() { return m_Depth; }
 
 		/// Getter for the size of this target.
 		const FloatRect& GetSize() { return m_Size; }
@@ -41,8 +43,8 @@ namespace RTE {
 		GLuint m_FBO{0};
 
 	private:
-		Texture2D m_Texture{};
-		Texture2D m_Depth{};
+		std::shared_ptr<Texture> m_Texture{nullptr};
+		std::shared_ptr<DepthTexture> m_Depth{nullptr};
 		bool m_ColorTextureOwned{true};
 	};
 } // namespace RTE
