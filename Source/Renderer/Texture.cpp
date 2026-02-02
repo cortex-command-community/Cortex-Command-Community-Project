64ad94dc62f14808f60bcded1f42c652f0fb2fde
HeliumAnt <lesgoets@gmail.com>
2026-02-02 21:12:36 +0100

    implement in gl fo rRenderBatch

--- Source/Renderer/Texture.cpp
+++ Source/Renderer/Texture.cpp
@@ -1,12 +1,112 @@
 #include "Texture.h"
+#include "allegro.h"
+#include "GLCheck.h"
+#include "glad/gl.h"
 
 using namespace RTE;
 
-std::shared_ptr<DrawCall> Texture::Draw(Box source, Box dest, glm::vec2 origin, float angle, Color color) {
-	bool flipX = false;
-	bool flipY = false;
+Texture::Texture(): m_Dimensions() {
+	glGenTextures(1, &m_TextureID);
+}
 
-	if (source.IsEmpty() || dest.IsEmpty()) {
-		return nullptr;
+Texture::~Texture() {
+	glDeleteTextures(1, &m_TextureID);
+}
+
+Texture::Texture(GLuint textureId): m_TextureID(textureId), m_Dimensions() {
+	glBindTexture(GL_TEXTURE_2D, m_TextureID);
+	glGetTexLevelParameterfv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &m_Dimensions.w);
+	glGetTexLevelParameterfv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &m_Dimensions.h);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+Texture::Texture(FloatRect dimensions, int bitDepth) : m_Dimensions(std::move(dimensions)) {
+	glGenTextures(1, &m_TextureID);
+
+	Bind();
+
+	if (bitDepth == 8) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, m_Dimensions.w, m_Dimensions.h, 0, GL_RED, GL_UNSIGNED_BYTE, nullptr);
+		GLint swizzleMask[] = {GL_RED, GL_RED, GL_RED, GL_ONE};
+		glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
+	} else {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_Dimensions.w, m_Dimensions.h, 0, bitDepth == 32 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+
 	}
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::Bind() {
+	glBindTexture(GL_TEXTURE_2D, m_TextureID);
+}
+
+BitmapTexture::BitmapTexture(std::unique_ptr<BITMAP, BitmapDeleter> bitmap, Filter filtering, WrapType clamp) : Texture(), m_Pixels(std::move(bitmap)), m_HaveAlpha(false) {
+	m_Dimensions = FloatRect(0.0f, 0.0f, m_Pixels->w, m_Pixels->h);
+	Bind();
+
+	int bitDepth = bitmap_color_depth(m_Pixels.get());
+	if (bitDepth == 8) {
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, m_Pixels->w, m_Pixels->h, 0, GL_RED, GL_UNSIGNED_BYTE, m_Pixels->dat);
+		GLint swizzleMask[] = {GL_RED, GL_RED, GL_RED, GL_ONE};
+		glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
+	} else {
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_Pixels->w, m_Pixels->h, 0, bitDepth == 32 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, m_Pixels->dat);
+	}
+
+	GLint wrap;
+	switch (clamp) {
+		case WrapType::ClampToEdge:
+			wrap = GL_CLAMP_TO_EDGE;
+			break;
+		case RTE::WrapType::Repeat:
+			wrap = GL_REPEAT;
+			break;
+	}
+
+	GLint textureMagFilter;
+	GLint textureMinFilter;
+	switch (filtering) {
+		case Filter::Linear:
+			textureMagFilter = GL_LINEAR;
+			textureMinFilter = GL_LINEAR;
+			break;
+		case Filter::LinearMipmap:
+			textureMagFilter = GL_LINEAR;
+			textureMinFilter = GL_LINEAR_MIPMAP_LINEAR;
+			break;
+		case Filter::Nearest:
+			textureMagFilter = GL_NEAREST;
+			textureMinFilter = GL_NEAREST;
+			break;
+	}
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, textureMagFilter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, textureMinFilter);
+
+	if (filtering == Filter::LinearMipmap) {
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+DepthTexture::DepthTexture(FloatRect dimensions) : Texture(std::move(dimensions)) {
+	Bind();
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, m_Dimensions.w, m_Dimensions.h, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
--- Source/Renderer/Texture.h
+++ Source/Renderer/Texture.h
@@ -7,22 +7,53 @@
 #include "DrawCall.h"
 #include "AllegroTools.h"
 
+extern "C" {
+typedef struct BITMAP BITMAP;
+}
+
 namespace RTE {
 	/// Abstraction for rectangular textures.
 	class Texture {
 	public:
-		Texture(std::unique_ptr<BITMAP, BitmapDeleter> bitmap);
-		BITMAP* GetBitmap() const { return m_Pixels.get(); }
+		Texture();
+		Texture(GLuint textureId);
+		Texture(FloatRect dimensions, int bitDepth = 32);
+		virtual ~Texture();
+		void Bind();
 		GLuint GetTextureId() const { return m_TextureID; }
 		const FloatRect& GetDimensions() const { return m_Dimensions; }
 
-	private:
+	protected:
 		GLuint m_TextureID{0};
 		FloatRect m_Dimensions;
+	};
+
+	enum class Filter {
+		Linear,
+		LinearMipmap,
+		Nearest
+	};
+	enum class WrapType {
+		ClampToEdge,
+		Repeat
+	};
+	class BitmapTexture : public Texture {
+	public:
+		BitmapTexture(std::unique_ptr<BITMAP, BitmapDeleter> bitmap, Filter filtering = Filter::Linear, WrapType clamp = WrapType::ClampToEdge);
+		~BitmapTexture() = default;
+		BITMAP* GetBitmap() const { return m_Pixels.get(); }
+
+	private:
 		std::unique_ptr<BITMAP, BitmapDeleter> m_Pixels;
 		bool m_HaveAlpha;
 
 	public:
-		//explicit operator BITMAP*() { return m_Pixels.get(); }
+		// explicit operator BITMAP*() { return m_Pixels.get(); }
+	};
+
+	class DepthTexture : public Texture {
+	public:
+		DepthTexture(FloatRect dimensions);
+		~DepthTexture() = default;
 	};
 } // namespace RTE
