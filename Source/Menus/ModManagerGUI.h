1f0f49eb69463f7ef500a7867523b68ed1fb9e03
getcetc <getcetc@proton.me>
2026-02-10 16:07:49 +0700

    ModManagerGUI redo v2

--- Source/Menus/ModManagerGUI.h
+++ Source/Menus/ModManagerGUI.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "DataModule.h"
+
 #include <string>
 #include <vector>
 #include <memory>
@@ -12,6 +14,9 @@ namespace RTE {
 	class GUILabel;
 	class GUIButton;
 	class GUIListBox;
+	class GUITab;
+	class GUICollectionBox;
+	class GUIBitmap;
 
 	/// Integrated mod and script manager user interface composition and handling.
 	class ModManagerGUI {
@@ -21,8 +26,7 @@ namespace RTE {
 		/// Constructor method used to instantiate a ModManagerGUI object in system memory and make it ready for use.
 		/// @param guiScreen Pointer to a GUIScreen interface that will be used by this ModManagerGUI's GUIControlManager. Ownership is NOT transferred!
 		/// @param guiInput Pointer to a GUIInput interface that will be used by this ModManagerGUI's GUIControlManager. Ownership is NOT transferred!
-		/// @param createForPauseMenu Whether this SettingsGUI is part of ModManagerGUI and should have a slightly different layout.
-		ModManagerGUI(AllegroScreen* guiScreen, GUIInputWrapper* guiInput, bool createForPauseMenu = false);
+		ModManagerGUI(AllegroScreen* guiScreen, GUIInputWrapper* guiInput);
 #pragma endregion
 
 #pragma region Concrete Methods
@@ -37,25 +41,69 @@ namespace RTE {
 	private:
 		/// Struct containing information about a valid mod DataModule.
 		struct ModRecord {
-			std::string ModulePath; //!< Mod DataModule path.
-			std::string ModuleName; //!< Mod ModuleName.
-			std::string Description; //!< Mod description.
-			bool Disabled; //!< Whether the mod is disabled through the settings file or not.
+			const DataModule* dataModule;
+			bool Enabled;
+			int InternalModuleIndex;
+
+			//!< Contructor for a mod DataModule that WAS loaded at launch.
+			ModRecord(const DataModule* dataModule, int internalModuleIndex) : 
+				dataModule(dataModule) 
+			{
+				Loaded = true;
+				Enabled = true;
+				InternalModuleIndex = internalModuleIndex;
+			}
+			//!< Contructor for a mod DataModule that was NOT loaded at launch.
+			ModRecord(const std::string& dotRTEName) :
+			    dataModule(nullptr), DotRTENameIfWasntLoaded(dotRTEName) 
+			{
+				Loaded = false;
+				Enabled = false;
+				InternalModuleIndex = -1;
+			}
+			//!< Mod DataModule path.
+			const std::string& GetDotRTEName() const {
+				return Loaded ? dataModule->GetFileName() : DotRTENameIfWasntLoaded;
+			}
+			//!< Mod ModuleName.
+			const std::string& GetFriendlyName() const {
+				return Loaded ? dataModule->GetFriendlyName() : "";
+			}
+			//!< Mod description.
+			const std::string GetDescription() const {
+				if (!Loaded) {
+					return "N/A, Module was not loaded";
+				} else {
+					std::string authorString =
+					    dataModule->GetAuthor() == ""
+					        ? ""
+					        : "Author: " + dataModule->GetAuthor() + "\n\n";
+					return std::move(authorString) + dataModule->GetDescription();
+				}
+			}
 
 			/// Makes GUI displayable string with mod info.
 			/// @return String with mod info.
-			std::string GetDisplayString() const { return (Disabled ? "- " : "+ ") + ModulePath + " - " + ModuleName; }
+			std::string GetDisplayString() const { 
+				return (Enabled ? "+ " : "- ") + GetDotRTEName(); 
+			}
 
 			/// Comparison operator for sorting the KnownMods list alphabetically by path with std::sort.
 			/// @param rhs ModRecord to compare with.
 			/// @return Bool with result of the alphabetical comparison.
-			bool operator<(const ModRecord& rhs) const { return ModulePath < rhs.ModulePath; }
+			bool operator<(const ModRecord& rhs) const { return GetDotRTEName() < rhs.GetDotRTEName(); }
+
+		private:
+			//!< Whether the mod DataModule was loaded at game launch.
+			bool Loaded;
+			//!< Mod DataModule's name string for if it was NOT loaded at game launch.
+			std::string DotRTENameIfWasntLoaded;
 		};
 
 		/// Struct containing information about a valid GlobalScript.
 		struct ScriptRecord {
 			std::string DisplayName;
-			std::string ModuleAndPresetName;
+			std::string ModuleAndPresetName; //!< The .rte name.
 			std::string Description; //!< Script description.
 			bool Enabled; //!< Whether the script is enabled through the settings file or not.
 
@@ -76,10 +124,12 @@ namespace RTE {
 			std::string Description;
 			std::vector<ScriptRecord> Records;
 			bool Collapsed = false;
+			int InternalModuleIndex;
 
-			ScriptRecordsInAModule(std::string moduleName, std::string description) {
+			ScriptRecordsInAModule(std::string moduleName, std::string description, int internalModuleIndex) {
 				ModuleName = moduleName;
 				Description = description;
+				InternalModuleIndex = internalModuleIndex;
 			}
 
 			ScriptRecord& operator[](int i) {
@@ -102,14 +152,28 @@ namespace RTE {
 
 		/// GUI elements that compose the Mod Manager menu screen.
 		GUIButton* m_BackToMainButton;
-		GUIButton* m_ToggleModButton;
-		GUIButton* m_ToggleScriptButton;
+		GUIButton* m_OpenModsFolderButton;
+		GUIButton* m_ToggleListEntryButton;
 		GUIListBox* m_ModsListBox;
 		GUIListBox* m_ScriptsListBox;
 		GUILabel* m_ModOrScriptDescriptionLabel;
+		GUIButton* m_ModsTabButton;
+		GUIButton* m_ScriptsTabButton;
+		GUILabel* m_ModuleHeaderLabel;
+		GUICollectionBox* m_ModuleIconCollectionBox;
 
 		//todo
-		std::string m_DisclaimerText;
+		const std::string& GetDisclaimerText() {
+			static const std::string disclaimerText = 
+				"DISCLAIMER: mods and any global scripts may cause performance degradation, unbalancing, game exploitation, crashes and extreme fun. Use wisely and be prepared to face the consequences.";
+			return disclaimerText;
+		}
+
+		enum ModManagerTab {
+			Mods,
+			Scripts
+		};
+		ModManagerTab m_CurrentTab;
 
 #pragma region Mod and Script Handling
 		/// Gets whether both lists were fetched, even if nothing valid was added to them.
@@ -130,11 +194,15 @@ namespace RTE {
 
 		void ResetSelectionsAndGoToTop();
 
+		void OpenModsFolder();
+
 		/// Turns currently selected mod on and off and changes GUI elements accordingly.
 		void ToggleMod();
 
 		//todo
 		ScriptRecord* ScriptListExtraIndexToScriptRecord(int);
+
+		void UpdateModuleHeader(const int moduleIndex);
 
 		/// todo Turns currently selected script on and off and changes GUI elements accordingly.
 		void ToggleInScriptList();
