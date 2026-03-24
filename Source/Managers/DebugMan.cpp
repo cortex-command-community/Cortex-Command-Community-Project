fb8225840f2a5ffd7577884997d3e637d78b6c2d
HeliumAnt <lesgoets@gmail.com>
2026-03-24 19:31:00 +0100

    add DebugMan impl

--- Source/Managers/DebugMan.cpp
+++ Source/Managers/DebugMan.cpp
@@ -1,5 +1,7 @@
 #include "DebugMan.h"
+#include "PerformanceMan.h"
 #include "imgui/imgui.h"
+#include "tracy/Tracy.hpp"
 
 using namespace RTE;
 
@@ -7,16 +9,98 @@ void Draw() {
 }
 
 void DebugMan::DrawImGui() {
-	DebugOptionsGUI();
+	if (m_ShowDebugWindow) {
+		DebugOptionsGUI();
+	}
+
+	if (m_ShowActorDebugGui) {
+		ActorDrawDebugGUI();
+	}
+
+	if (m_ImGuiDemoWindow) {
+		ImGui::ShowDemoWindow(&m_ImGuiDemoWindow);
+	}
+
+	if (m_ShowPerformanceMan) {
+		g_PerformanceMan.ImGui();
+	}
 }
 
 void DebugMan::DebugOptionsGUI() {
 	if (ImGui::Begin("Debug Options", &m_ShowDebugWindow)) {
+		ImGui::Checkbox("Show Performance Stats", &m_ShowPerformanceMan);
+		ImGui::Checkbox("Show ImGui Demo Window", &m_ShowDebugWindow);
+		ImGui::Checkbox("Show Actor debug", &m_ShowActorDebugGui);
 		if (ImGui::TreeNode("Debug Draw")) {
 			ImGui::Checkbox("Draw Camera bounds", &m_DrawCameraBounds);
-			if (ImGui::TreeNode("")) {
 
+			if (ImGui::TreeNode("Sprite Draw")) {
+				ImGui::Checkbox("Draw frustum tests", &m_DrawSpriteBounds);
+				ImGui::TreePop();
 			}
+			ImGui::TreePop();
+		}
+	}
+	ImGui::End();
+}
+
+#include "Draw.h"
+#include "RenderTarget.h"
+#include "RenderBatch.h"
+#include "RenderMan.h"
+#include "MovableMan.h"
+#include "CameraMan.h"
+#include "SceneMan.h"
+#include "tracy/TracyOpenGL.hpp"
+
+void DebugMan::ActorDrawDebugGUI() {
+	ZoneScoped;
+	static std::shared_ptr<RenderBatch> batch = std::make_unique<RenderBatch>();
+	if (ImGui::Begin("Actor Draw Debug", &m_ShowActorDebugGui)) {
+		static std::map<MovableObject*, std::unique_ptr<Texture>> MOTargets;
+		static int playerScreen = -1;
+		ImGui::InputInt("Test Draw for Screen (-1 full world):", &playerScreen);
+		ImGui::SliderInt("Screen", &playerScreen, -1, c_MaxScreenCount);
+		if (g_SceneMan.GetScene()) {
+			ZoneScopedN("ActorList");
+			g_RenderMan.SetActiveBatch(batch.get());
+			g_RenderMan.BeginFrame();
+			if (playerScreen >= 0) {
+				ZoneScopedN("ActorList::DrawPlayer");
+				for (auto& camera: g_CameraMan.GetPlayerCameras(playerScreen)) {
+					g_MovableMan.Draw(camera);
+				}
+			} else {
+				ZoneScopedN("ActorList::DrawAll");
+				Camera camera{{0.0f, 0.0f}, {{0.0f, 0.0f}, (float)g_SceneMan.GetSceneHeight(), (float)g_SceneMan.GetSceneWidth()}};
+				g_MovableMan.Draw(camera);
+			}
+			g_RenderMan.ResetActiveBatch();
+			batch->EndFrame();
+			{
+				ZoneScopedN("ActorList::List");
+				for (auto actor: g_MovableMan.m_Actors) {
+					if (ImGui::TreeNode(actor->GetPresetNameAndUniqueID().c_str())) {
+						ZoneScopedN("ActorList::List::Node");
+						if (!MOTargets[actor]) {
+							MOTargets[actor] = std::make_unique<Texture>(FloatRect{0.0f, 0.0f, actor->GetRadius() * 2.0f, 2.f * actor->GetRadius()});
+						}
+						if (!m_DebugDrawTarget) {
+							m_DebugDrawTarget = std::make_unique<RenderTarget>(false);
+						}
+						m_DebugDrawTarget->Begin(true, false);
+						glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, MOTargets[actor]->GetTextureId(), 0);
+						glViewport(0, 0, 2 * actor->GetRadius(), 2 * actor->GetRadius());
+						Camera camera(actor->GetPos() - Vector(actor->GetRadius(), actor->GetRadius()), Box({0.0f, 0.0f}, actor->GetRadius() * 2, actor->GetRadius() * 2.0f));
+						batch->m_CurrentCamera = &camera;
+						batch->Render();
+						glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+						ImGui::ImageWithBg(MOTargets[actor]->GetTextureId(), ImVec2(MOTargets[actor]->GetDimensions().w, MOTargets[actor]->GetDimensions().h));
+						ImGui::TreePop();
+					}
+				}
+			}
+			batch->ClearDraws();
 		}
 	}
 	ImGui::End();
--- Source/Managers/DebugMan.h
+++ Source/Managers/DebugMan.h
@@ -5,21 +5,39 @@
 #define g_DebugMan DebugMan::Instance()
 
 namespace RTE {
+	class RenderTarget;
 	class DebugMan : public Singleton<DebugMan> {
+		friend class SettingsMan;
 	public:
 		void Draw();
 		void DrawImGui();
 
+		void ShowDebugOptions() { m_ShowDebugWindow = true; }
+
 		bool DrawSpriteBounds() { return m_DrawSpriteBounds; }
 		constexpr bool DrawNoGravBoxes() { return false; }
 		bool DrawBigTextureBounds() { return false; }
+		bool DrawTilingBounds() { return true; }
+
+		bool FreeCamEnabled() { return m_EnableFreeCam; }
+		float FreeCamZoom() { return m_FreeCamZoom; }
 
 	private:
 		bool m_ShowDebugWindow{false};
+		bool m_ImGuiDemoWindow{false};
+		bool m_ShowPerformanceMan{false};
 
 		bool m_DrawCameraBounds{false};
 		bool m_DrawSpriteBounds{false};
 
 		void DebugOptionsGUI();
+
+		bool m_ShowActorDebugGui{false};
+		std::unique_ptr<RenderTarget> m_DebugDrawTarget;
+		void ActorDrawDebugGUI();
+
+
+		bool m_EnableFreeCam{false};
+		float m_FreeCamZoom{1.0f};
 	};
 } // namespace RTE
